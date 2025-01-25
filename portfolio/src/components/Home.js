2a36feb1d72046961a4d140c8ce497e258a04dca
Jiteen Mohanty <86570013+JiteenMohanty@users.noreply.github.com>
2025-01-25 21:45:44 +0530

    TestRun1

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -4,7 +4,9 @@ const Home = () => {
     return (
         <div className="landing">
             <h1>Welcome to My Portfolio</h1>
-            <p>Hi, I'm [Your Name], a passionate developer.</p>
+            <p>Hi, I'm Jiteen Mohanty, a passionate developer.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <p>Explore my work and get to know me better!</p>
             <nav>
                 <ul>
